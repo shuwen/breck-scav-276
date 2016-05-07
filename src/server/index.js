522cdf0b0147669c3f80e45e6c4c8f341a9c84ea
Shuwen Qian <shuwenq@gmail.com>
2016-05-07 08:47:00 -0500

    allow content-type header

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.all('/', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
 	next();
 });
 
